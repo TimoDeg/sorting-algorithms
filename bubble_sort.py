7f2e76a33647d116546366049338582b5cbd369e
Tima <timofeydegtyarev1@gmail.com>
2025-07-14 17:53:46 +0200

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+nums = [ 0, 1, 4, 2, 6, 3, 8, 9, 9, 5]
+nums_len = len(nums) - 1
+
+
+for i in range(len(nums)):
+    for j in range(len(nums) -1 - i):
+        current_num = nums[j]
+        next_num = nums[j + 1]
+
+        if (current_num > next_num):
+            nums[j] = next_num
+            nums[j + 1] = current_num
+            
+        
+
+print(nums)    
